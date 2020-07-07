a76ba7d39fadca1afc53d7df601fe62851fab956
Dennis Hagens <root@ipaddr.nl>
2020-07-07 08:18:06 +0000

    Update output.tf

--- output.tf
+++ output.tf
@@ -1,8 +1,9 @@
 output "vpc" {
+    description = "The created VPC with all of it's attributes"
     value = aviatrix_vpc.default
 }
 
 output "transit_gateway" {
+    description = "The Aviatrix transit gateway object with all of it's attributes"
     value = var.ha_gw ? aviatrix_transit_gateway.ha[0] : aviatrix_transit_gateway.single[0]
-    #value = aviatrix_transit_gateway.single
 }
