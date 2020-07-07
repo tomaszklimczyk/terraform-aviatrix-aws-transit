77d33cb4b7fd93946e155e1ceecaabb620fc4589
Dennis Hagens <root@ipaddr.nl>
2020-07-07 08:18:59 +0000

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,22 +1,27 @@
 variable "region" {
+    description = "The AWS region to deploy this module in"
     type = string
     default = "eu-west-1"
 }
 
 variable "cidr" {
+    description = "The CIDR range to be used for the VPC"
     type = string
 }
 
 variable "aws_account_name" {
+    description = "The AWS account name, as known by the Aviatrix controller"
     type = string
 }
 
 variable "instance_size" {
+    description = "AWS Instance size for the Aviatrix gateways
     type = string
     default = "t2.micro"
 }
 
 variable "ha_gw" {
+    description = "Boolean to determine if module will be deployed in HA or single mode"
     type = bool
     default = false
 }
